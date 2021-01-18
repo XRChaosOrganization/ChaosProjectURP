95995b6389f9dec5ca3a28a654c9c202b6212f41
Ben <benjamin.matheron@hotmail.fr>
2021-01-18 20:33:33 +0100

    Autotarget Closest

---
+++ Chaos/Assets/Scripts/RPG/Enemies/Target.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    #region Go or components
+
+    private SpriteRenderer target;
+
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Inputs
+
+    private void OnEnable()
+    {
+        Targeting.ClearTargets += TargetOff;
+    }
+
+    private void OnDisable()
+    {
+        Targeting.ClearTargets -= TargetOff;
+    }
+
+    #endregion
+
+    #region Variables
+    #endregion
+
+    #region Unity loop
+    void Awake()
+    {
+        target = GetComponentInChildren<SpriteRenderer>();
+        TargetOff();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+	void FixedUpdate()
+    {
+        
+    }
+	
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Methods
+
+    private void TargetOff()
+    {
+        target.gameObject.SetActive(false);
+    }
+
+    public void TargetOn()
+    {
+        target.gameObject.SetActive(true);
+    }
+
+    #endregion
+}
